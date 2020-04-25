8986c091c69c3bd5a0a9d71f8e237ea1adcf88cf
thms200 <thms0504@gmail.com>
2020-04-25 18:40:04 +0900

    feat: Add disconnect customer

--- config/rooms.js
+++ config/rooms.js
@@ -1,6 +1,6 @@
 const roomList = {};
 
-const arrangeRoom = (nickname, mode, consultant) => {
+const arrangeCustomerRoom = (nickname, mode, consultant) => {
   const trimmedName = nickname.trim();
   const customer = {};
   customer[trimmedName] = mode;
@@ -15,4 +15,24 @@ const arrangeRoom = (nickname, mode, consultant) => {
   return trimmedName;
 };
 
-module.exports = { roomList, arrangeRoom };
+const removeCustomerRoom = (nickname, consultant) => {
+  const trimmedName = nickname.trim();
+
+  let leftCustomer = [];
+  for (let seller in roomList) {
+    if (consultant === seller) {
+      const customers = roomList[seller];
+      for (let i = 0; i < customers.length; i++) {
+        if (Object.keys(customers[i])[0] === trimmedName) {
+          leftCustomer = customers.splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  return Object.keys(leftCustomer[0])[0];
+};
+
+module.exports = { roomList, arrangeCustomerRoom, removeCustomerRoom };
+
--- config/socket.js
+++ config/socket.js
@@ -1,11 +1,21 @@
-const { arrangeRoom } = require('./rooms');
+const { arrangeCustomerRoom, removeCustomerRoom } = require('./rooms');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('joinCustomer', (nickname, mode, consultant) => {
+    socket.on('joinCustomer', (nickname, mode, consultant, callback) => {
       try {
-        const room = arrangeRoom(nickname, mode, consultant);
+        const room = arrangeCustomerRoom(nickname, mode, consultant);
         socket.join(room);
+        callback(`${nickname}님, 잠시만 기다려주시면 상담을 시작하겠습니다.`);
+      } catch (error) {
+        console.warn(error);
+      }
+    });
+
+    socket.on('disconnectionCustomer', (nickname, consultant, callback) => {
+      try {
+        const customer = removeCustomerRoom(nickname, consultant);
+        callback(`${customer}님, 이용해 주셔서 감사합니다.`);
       } catch (error) {
         console.warn(error);
       }
