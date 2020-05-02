114f87f3d70d7e504a0d313e4fe17d519b7e339c
thms200 <thms0504@gmail.com>
2020-05-03 00:29:29 +0900

    refactor: Modify authorization(error constants)

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -5,11 +5,7 @@ const { ERROR } = require('../constants');
 const ensureAuthenticated = async(req, res, next) => {
   try {
     const token = req.headers['x-access-token'].split('Bearer')[1].trim();
-<<<<<<< HEAD
     if (!token) return next(createError(401), ERROR.INVALID_TOKEN);
-=======
-    if (!token) return next(createError(401), errorMsg.invalidToken);
->>>>>>> 66294997af0180e5e1ad5ac44f9330c828d348ca
     const secretKey = process.env.SECRET_KEY;
     const payload = await jwt.verify(token, secretKey);
     res.locals.userInfo = payload;
